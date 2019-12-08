0bc686e8f45daecda60cd6c7ca451989a68eeaa6
Gustavo Teixeira de Castro <gustavoteixeiradecastro@MacBook-Air-de-Gustavo.local>
2019-12-08 19:32:55 -0300

    Criando page 404, inserndo imagens e início dev quiz

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Page404 from '../views/Page404.vue'
+import Quiz from '../views/Quiz.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/quiz/:gId',
+    name: 'Quiz',
+    component: Quiz
+  },
+  { 
+    path: "*", 
+    component: Page404 
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Page404.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="msg-404">
+        <div class="content">
+            <div class="title">
+                Sorry, the page you are looking for could not be found. 
+            </div>
+            <router-link to='/' class="btn btn-primary">Back</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .msg-404{
+        height: 100vh;
+        width: 100vw;
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+    .title {
+        font-size: 36px;
+        padding: 20px;
+        font-family: 'Lato', sans-serif;
+        font-weight: 100;
+    }
+    .btn{
+        display: block;
+        max-width: 150px;
+        margin: 0 auto;
+        line-height: 1;
+    }
+</style>
