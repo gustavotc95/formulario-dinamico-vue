e26fa5a707f8d8457f2f53c8664ab60995d06aaf
Gustavo Teixeira <gustavotc95@gmail.com>
2019-12-08 12:25:20 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="landing">
+      <div class="container">
+        <img src="../assets/images/logo.svg" alt="KetoCycle.Diet" class="regular-logo">
+
+        <h1>
+          Get your personalized 28-day Keto diet
+        </h1>
+
+        <div class="gender-description">
+          Select your gender:
+        </div>
+
+        <div class="gender-buttons">
+          <button class="btn btn-female" data-gender="female">
+            <span class="icon"><i class="female"></i></span>
+              Female
+            <i class="forward-female forward"></i>
+          </button>
+          <button class="btn btn-male" data-gender="male">
+            <span class="icon"><i class="male"></i></span>
+              Male
+            <i class="forward-male forward"></i>
+          </button>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-6 links col-lg-4">
+            <a href="#">F.A.Q</a>
+            <a href="#"><strong>Access my plan</strong></a>
+            <a href="#">General conditions</a>
+            <a href="#">Data protection policy</a>
+            <a href="#">Contact us</a>
+          </div>
+
+          <div class="col-xs-6 col-lg-4">
+            <img src="../assets/images/single-color-logo.svg" class="footer-logo">
+          </div>
+
+          <div class="col-xs-12 col-lg-4 copyright">
+            2019 © All rights reserved
+            <div class="disclaimer">
+              DISCLAIMER: Results may vary from person to person
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
